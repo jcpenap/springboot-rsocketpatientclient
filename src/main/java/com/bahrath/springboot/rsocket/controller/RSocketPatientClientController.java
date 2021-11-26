13aa9a87b4ef688224cd6885233c858a0e603b02
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-26 15:51:11 -0500

    Adding Request controller

--- src/main/java/com/bahrath/springboot/rsocket/controller/RSocketPatientClientController.java
+++ src/main/java/com/bahrath/springboot/rsocket/controller/RSocketPatientClientController.java
@@ -1,16 +1,32 @@
 package com.bahrath.springboot.rsocket.controller;
 
+import com.bahrath.springboot.rsocket.model.ClinicalPatientData;
+import com.bahrath.springboot.rsocket.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class RSocketPatientClientController {
+
+    Logger logger = LoggerFactory.getLogger(RSocketPatientClientController.class);
 
     private final RSocketRequester rSocketRequester;
 
     public RSocketPatientClientController(@Autowired RSocketRequester.Builder builder) {
         this.rSocketRequester = builder.tcp("localhost", 7000);
+    }
+
+    @GetMapping("/request-response")
+    public Mono<ClinicalPatientData> requestResponse(Patient patient) {
+        logger.info("Sending the rsocket request for patient: "+patient);
+        return rSocketRequester.route("get-patient-data")
+                .data(patient)
+                .retrieveMono(ClinicalPatientData.class);
     }
 
 }
